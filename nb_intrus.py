9fdda181d05359b3e4aa90c68ad58240e3b5aba6
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-07-18 13:26:45 +0200

    Ajout de la fonction d'arret du programme

--- nb_intrus.py
+++ nb_intrus.py
@@ -18,16 +18,20 @@ def intruders_numbers():
 
     while True:
         try:
-            number_list = str(input("Veuillez saisir une liste de nombre entier séparé d'un espace : "))
+            number_list = str(input("Veuillez saisir une liste de nombre entier séparé d'un espace. Ou saisir 0 pour terminer : "))
 
-            if  len(number_list) == 0 : #Vérifie si la liste est vide
-                print("Votre liste est vide", len(number_list))
+            if number_list == "0":
+                print("Fin du programme. Merci ! ")
                 break
 
+            if len(number_list) == 0 : #Vérifie si la liste est vide
+                print("Votre liste est vide", len(number_list))
+                break
             else:
                 #Sépare chaque nombre en utilisant l'espace et crée une liste de valeurs entières
                 number_list = [int(x) for x in number_list.split(" ")]
             print("Votre liste est : ", number_list)
+
 
 
         except ValueError:
