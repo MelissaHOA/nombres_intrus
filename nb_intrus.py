0372db7c6d0b2dd6f68dfa04cd4d03b95586529d
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-07-18 13:14:00 +0200

    premiere fonction : saisie d'une liste par l'utilisateur

--- nb_intrus.py
+++ nb_intrus.py
@@ -3,6 +3,34 @@
 
 """
 Exercice 1.a : à la recherche du nombre intrus
+Écrire un programme qui détecte dans une liste contenant des nombres entiers
+non nuls des intrus qui sont des nombres négatifs ;
+le programme doit être composé des versions successives suivantes :
 """
 
-print("Nombres intrus")
+
+"""
+    1. Demander à l’utilisateur chaque nombre constituant la liste ; par convention, 
+    la saisie de 0 indique que la saisie de la liste est terminée
+"""
+
+def intruders_numbers():
+
+    while True:
+        try:
+            number_list = str(input("Veuillez saisir une liste de nombre entier séparé d'un espace : "))
+
+            if  len(number_list) == 0 : #Vérifie si la liste est vide
+                print("Votre liste est vide", len(number_list))
+                break
+
+            else:
+                #Sépare chaque nombre en utilisant l'espace et crée une liste de valeurs entières
+                number_list = [int(x) for x in number_list.split(" ")]
+            print("Votre liste est : ", number_list)
+
+
+        except ValueError:
+            print("Une des valeurs saisie n'est pas un nombre entier")
+
+intruders_numbers()
