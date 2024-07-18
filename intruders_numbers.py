da1d41fe6a82295f55b79d4da62eabb06d6ad8b1
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-07-18 13:42:10 +0200

    Création du fichier de l'exercice

---
+++ intruders_numbers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Exercice 1.a : à la recherche du nombre intrus
+Écrire un programme qui détecte dans une liste contenant des nombres entiers
+non nuls des intrus qui sont des nombres négatifs ;
+le programme doit être composé des versions successives suivantes :
+"""
+
+
+"""
+    1. Demander à l’utilisateur chaque nombre constituant la liste ; par convention, 
+    la saisie de 0 indique que la saisie de la liste est terminée
+"""
