f456260eb118b01c73dc6a85e00eb6a53c147b02
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-07-19 11:32:22 +0200

    1.déporter dans une fonction input_list_numbers  la partie du code chargée de permettre à l’utilisateur  de saisir la liste de nombres entiers de départ

--- intruders_numbers.py
+++ intruders_numbers.py
@@ -5,54 +5,111 @@
 # Écrire un programme qui détecte dans une liste contenant des nombres entiers
 # non nuls des intrus qui sont des nombres négatifs
 #
+# Exercice 1.b : mise en fonction de la recherche du nombre intrus
+# Il sera continué dans le même projet PyCharm et hébergé par le même dépôt Git.
+#
 
-
-def intruders_nbr():
+def input_list_numbers():
+    """Fonction pour saisir la liste de nombres entiers de départ."""
     user_number_list = []
-    count_intruders = 0
-    intruders_list = []
-    number_positif_list = []
-
-    """Demande à l'utilisateur de saisir un entier et de trouver l'intrus.
-    
-        :param user-number : message d'invite à la saisie
-        :param user_number_list : liste de tous les nombres saisi
-        :param number_positif_list : liste de tous les nombres positifs saisis
-        :param intruders_list : Liste de tous les intrus saisis
-        :param count_intruders : compteurs des intrus
-        :return: les détails saisis
-        
-    """
-
     while True:
         try:
-
-            user_number = int(input("Trouvez l'intrus : Veuillez saisir un nombre entier : "))
-
-            # Stock les nombres
-            if user_number != 0:
-                user_number_list.append(user_number)
-
-                # Stock les nombres
-                if user_number > 0:
-                    number_positif_list.append(user_number)
-
-                # Stock les nombres et compte
-                elif user_number < 0:
-                    count_intruders = count_intruders + 1
-                    intruders_list.append(user_number)
-
-            else:
-                if count_intruders == 0:
-                    print(f"Vous n'avez pas trouvé d'intrus. Voici les nombres saisi : {user_number_list}")
-                else:
-                    print(f"Le nombre d'intrus détecté est de : {count_intruders} , voici la liste : {intruders_list}")
-                    print(f"Les nombres sans intrus sont :  {number_positif_list}")
-                    print(f"Tous les nombres saisi sont :  {user_number_list}")
+            user_number = int(input("Trouvez l'intrus : Veuillez saisir un nombre entier : (0 pour terminer) : "))
+            if user_number == 0:
+                break
+            user_number_list.append(user_number)
 
         except ValueError:
-            print("La valeur saisie n'est pas valide")
+            print("La valeur saisie n'est pas valide. Veuillez saisir un nombre entier. ")
+    return user_number_list
+#
+# def detect_numbers_intruder():
+#     count_intruders = 0
+#     intruders_list = []
+#     number_positif_list = []
+#
+#     # Stock et compte les nombres négatifs
+#     if user_number < 0:
+#         count_intruders += 1
+#         intruders_list.append(user_number)
+#
+#     # Stock les nombres positifs
+#     if user_number > 0:
+#         number_positif_list.append(user_number)
+#
+#
+#
 
 
-intruders_nbr()
+# def intruders_nbr():
+#
+#     while True:
+#         try:
+#             # Stock tous les nombres saisis
+#             if user_number != 0:
+#                 user_number_list.append(user_number)
+#
+#             else:
+#                 if count_intruders == 0:
+#                     print(f"Vous n'avez pas trouvé d'intrus. Voici les nombres saisi : {user_number_list}")
+#                 else:
+#                     print(f"Le nombre d'intrus détecté est de : {count_intruders} , voici la liste : {intruders_list}")
+#                     print(f"Les nombres sans intrus sont :  {number_positif_list}")
+#                     print(f"Tous les nombres saisi sont :  {user_number_list}")
+#
+#         except ValueError:
+#             print("La valeur saisie n'est pas valide")
+
+
+# intruders_nbr()
+
+
+# def intruders_nbr():
+#     user_number_list = []
+#     count_intruders = 0
+#     intruders_list = []
+#     number_positif_list = []
+#
+#     """Demande à l'utilisateur de saisir un entier et de trouver l'intrus.
+#
+#         :param user-number : message d'invite à la saisie
+#         :param user_number_list : liste de tous les nombres saisi
+#         :param number_positif_list : liste de tous les nombres positifs saisis
+#         :param intruders_list : Liste de tous les intrus saisis
+#         :param count_intruders : compteurs des intrus
+#         :return: les détails saisis
+#
+#     """
+#
+#     while True:
+#         try:
+#
+#             user_number = int(input("Trouvez l'intrus : Veuillez saisir un nombre entier : "))
+#
+#             # Stock les nombres
+#             if user_number != 0:
+#                 user_number_list.append(user_number)
+#
+#                 # Stock les nombres
+#                 if user_number > 0:
+#                     number_positif_list.append(user_number)
+#
+#                 # Stock les nombres et compte
+#                 elif user_number < 0:
+#                     count_intruders = count_intruders + 1
+#                     intruders_list.append(user_number)
+#
+#             else:
+#                 if count_intruders == 0:
+#                     print(f"Vous n'avez pas trouvé d'intrus. Voici les nombres saisi : {user_number_list}")
+#                 else:
+#                     print(f"Le nombre d'intrus détecté est de : {count_intruders} , voici la liste : {intruders_list}")
+#                     print(f"Les nombres sans intrus sont :  {number_positif_list}")
+#                     print(f"Tous les nombres saisi sont :  {user_number_list}")
+#
+#         except ValueError:
+#             print("La valeur saisie n'est pas valide")
+#
+#
+# intruders_nbr()
 
