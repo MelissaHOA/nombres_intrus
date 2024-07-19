38815ba72c00f0959d41a87116591efe25e2a742
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-07-19 09:35:23 +0200

    Mise à jour et vérification du code

--- intruders_numbers.py
+++ intruders_numbers.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Exercice 1.a : à la recherche du nombre intrus
-Écrire un programme qui détecte dans une liste contenant des nombres entiers
-non nuls des intrus qui sont des nombres négatifs ;
-le programme doit être composé des versions successives suivantes :
-"""
+# Exercice 1.a : à la recherche du nombre intrus
+# Écrire un programme qui détecte dans une liste contenant des nombres entiers
+# non nuls des intrus qui sont des nombres négatifs ;
+# le programme doit être composé des versions successives suivantes :
 
 
 """
@@ -14,15 +12,14 @@ le programme doit être composé des versions successives suivantes :
     la saisie de 0 indique que la saisie de la liste est terminée
 """
 
-def intruders_nbr():
 
+def intruders_nbr():
     user_number_list = []
     count_intruders = 0
     intruders_list = []
     number_positif_list = []
 
-
-    while True :
+    while True:
         try:
 
             user_number = int(input("Trouvez l'intrus : Veuillez saisir un nombre entier : "))
@@ -41,17 +38,16 @@ def intruders_nbr():
                     intruders_list.append(user_number)
 
             else:
-                print(f"Le nombre d'intrus détecté est de : {count_intruders} , voici la liste : {intruders_list}")
-                print(f"Les nombres sans intrus sont :  {number_positif_list}")
-                print(f"Tous les nombres saisi sont :  {user_number_list}")
-
-
-
-
-
+                if count_intruders == 0:
+                    print(f"Vous n'avez pas trouvé d'intrus. Voici les nombres saisi : {user_number_list}")
+                else:
+                    print(f"Le nombre d'intrus détecté est de : {count_intruders} , voici la liste : {intruders_list}")
+                    print(f"Les nombres sans intrus sont :  {number_positif_list}")
+                    print(f"Tous les nombres saisi sont :  {user_number_list}")
 
         except ValueError:
             print("La valeur saisie n'est pas valide")
+
 
 intruders_nbr()
 
